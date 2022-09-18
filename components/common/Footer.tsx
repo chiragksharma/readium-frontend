b99d5e7a9998e7afb46dcadd58c546676cef8647
Vinit Kumar Shah <vinitkumar18072001@gmail.com>
2022-09-18 07:45:47 +0530

    initial commit

---
+++ components/common/Footer.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Footer = () => (
+  <footer>
+    <div className="container">
+      <a href="/" className="logo-font">
+      Readium
+      </a>
+      <span className="attribution">
+        An interactive learning project from{" "}
+        <a href="localhost:3000">Readium</a>. Code &amp; design licensed
+        under MIT.
+      </span>
+    </div>
+  </footer>
+);
+
+export default Footer;
